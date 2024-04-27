80fcbe5f090fd72d5412ada689decb82e2f5f28c
zhaosx <shaoxi2010@qq.com>
2024-04-27 23:37:02 +0800

    改进策略

--- gushiwen.py
+++ gushiwen.py
@@ -6,10 +6,9 @@ from selenium.common.exceptions import TimeoutException
 from dataclasses import dataclass
 from lxml import etree
 from re import search
-from os import path
+from os import path, mkdir
 
 import json
-import sys
 import pandas as pd
 
 
@@ -22,9 +21,7 @@ class GushiwenXPATH:
     PoemBtn = "//div[@class='searchleft']/a[text() = '诗文']"
     PoetBtn = "//div[@class='searchleft']/a[text() = '古籍']"
     AuthorBtn = "//div[@class='searchleft']/a[text() = '作者']"
-    Tile = "//div[@class='sons']//div[*]/a/text()"
-    Author = "//div[@class='sons']//div[*]/a/span/text()"
-    Link = "//div[@class='sons']//div[*]/a/@href"
+    poemItem = "//div[@class='sons']//div[*]/a"
     # 诗文页面
     Expand = "//a[contains(text(), '展开阅读全文')]"
 
@@ -40,6 +37,9 @@ class Gushiwen:
                 self.cookies = json.loads(f.read())
         else:
             self.login_password()
+
+    def quit(self):
+        self.driver.quit()
 
     def login_password(self):
         driver = get_browser_driver()
@@ -83,23 +83,32 @@ class Gushiwen:
         return selector
 
     def collect_poems(self) -> pd.DataFrame:
-        selector = self.qeury_collect()
-        titles = selector.xpath(GushiwenXPATH.Tile)
-        links = selector.xpath(GushiwenXPATH.Link)
-        authors = selector.xpath(GushiwenXPATH.Author)
-
         def extract_id(s):
             match = search(r"/shiwenv_(.*?).aspx", s)
             if match:
                 return int(match.group(1), 16)
             return None
 
-        database = zip(
-            map(extract_id, links),
-            titles,
-            map(lambda x: x.strip(" -"), authors),
-            map(lambda x: self.base_url + x.strip(), links),
-        )
+        def get_current_page():
+            data = []
+            items = selector.xpath(GushiwenXPATH.poemItem)
+            for item in items:
+                tile_author = "".join(item.itertext())
+                link = item.attrib["href"]
+                id = extract_id(link)
+                if id:
+                    tile, author = tile_author.split("-")
+                    data.append(
+                        (id, tile.strip(), author.strip(), self.base_url + link.strip())
+                    )
+                else:
+                    print(f"{tile_author} 没有id!!!")
+            return data
+
+        selector = self.qeury_collect()
+        database = []
+        database.extend(get_current_page())
+
         return pd.DataFrame(
             data=list(database),
             columns=["id", "title", "author", "link"],
@@ -111,33 +120,57 @@ class Gushiwen:
         for expand in self.driver.find_elements(By.XPATH, GushiwenXPATH.Expand):
             expand.click()
         selector = etree.HTML(self.driver.page_source)
-        content = selector.xpath(f"//div[@id='contson{id:012x}']/p/text()")
-        if not content:
-            content = selector.xpath(f"//div[@id='contson{id:012x}']/text()")
-        return map(lambda x: x.strip(), content)
+        contson = selector.xpath(f"//div[@id='contson{id:012x}']")
+        contsontext = "\n".join(contson[0].itertext()).strip()
+        return contsontext
 
     def pomes_content(self, df) -> pd.DataFrame:
-        ids = []
-        contents = []
+        data = []
         for _, row in df.iterrows():
-            print(row)
             id, link = row["id"], row["link"]
+            print(f"拉取诗文: {id} {link}")
             content = self.get_poem_content(id, link)
-            ids.append(id)
-            contents.append("\n".join(content))
+            data.append((id, content))
         return pd.DataFrame(
-            data=list(zip(ids, contents)),
+            data=data,
             columns=["id", "content"],
         )
+
+    def pandas_update_pomes(self):
+        self.login()
+        remote = self.collect_poems()
+        if not path.exists("data"):
+            mkdir("data")
+        remote.to_csv("data/pomes_index.csv", index=False)
+        if path.exists("data/pomes_data.csv"):
+            data = pd.read_csv("data/pomes_data.csv")
+            full = pd.merge(remote, data, on="id", how="left")
+            # 增量拉取
+            delta = full[full["content"].isnull()]
+            deltadata = self.pomes_content(delta)
+            data = pd.concat([data, deltadata], ignore_index=True)
+        else:
+            # 全局拉取
+            data = self.pomes_content(remote)
+        data.to_csv("data/pomes_data.csv", index=False)
+        full = pd.merge(remote, data, on="id")
+        self.quit()
+        return full
+
+    def pandas_pomes(self):
+        if path.exists("data/pomes_index.csv") and path.exists("data/pomes_data.csv"):
+            index = pd.read_csv("data/pomes_index.csv")
+            data = pd.read_csv("data/pomes_data.csv")
+            full = pd.merge(index, data, on="id", how="left")
+            if any(full["content"].isnull()):
+                return self.pandas_update_pomes()
+            return full
+        else:
+            data = self.pandas_update_pomes()
+            return data
 
 
 if __name__ == "__main__":
     gushiwen = Gushiwen()
-    gushiwen.login()
-    base = gushiwen.collect_poems()
+    base = gushiwen.pandas_pomes()
     print(base)
-    contents = gushiwen.pomes_content(base)
-    print(contents)
-    new = pd.merge(base, contents, on="id")
-    print(new)
-    sys.stdin.read()
